f4de78c8dd36f383af3a247d6bcfdaf3be9f7750
dakshjoshi02 <daksh.joshi@ericsson.com>
2023-04-20 15:15:29 -0400

    Interface UserManagement - AccountManagement
    Used reference to UserManagfement to the AccountManagement
    Now the number of associated accounts for the user is modified
    according to the addition/deletion of accounts for that user

--- src/server/UserManagement.java
+++ src/server/UserManagement.java
@@ -179,4 +179,13 @@ public class UserManagement implements PropertyChangeListener
       deleteUser(userName);
     }
   }
+
+  public void addAssociatedAccountsNo(User user) 
+  {
+    User userNumOfAssociatedAccounts = users.get(user.fullName);
+    if (user != null)
+    {
+      userNumOfAssociatedAccounts.numOfAssociatedAccounts += 1;
+    }
+  }
 }
